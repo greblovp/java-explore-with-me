452b998d0db90f5719dfe0213b192f5cf8e0a717
pavelgreblov <greblovpn@yandex.ru>
2023-07-21 21:45:12 +0300

    style:

--- ewm-statistics/ewm-statistics-dto/src/main/java/ru/practicum/ewm/dto/EndpointHitDto.java
+++ ewm-statistics/ewm-statistics-dto/src/main/java/ru/practicum/ewm/dto/EndpointHitDto.java
@@ -2,9 +2,9 @@ package ru.practicum.ewm.dto;
 
 import lombok.Builder;
 import lombok.Data;
+
 import javax.validation.constraints.NotBlank;
 import javax.validation.constraints.NotNull;
-import java.time.LocalDateTime;
 
 @Data
 @Builder
